3fb3470ecc36f2dda90fd6ec57e58f946c914911
eko <ekotime.sun@gmail.com>
2024-09-17 14:39:52 +0800

    Refactor SecurityConfig to use SecurityFilterChain
    - Update SecurityConfig to use SecurityFilterChain instead of WebSecurityConfigurerAdapter
    - Remove extends WebSecurityConfigurerAdapter
    - Replace configure methods with @Bean SecurityFilterChain
    - Update security configuration to use lambda expressions
    - Improve code readability and maintainability

--- mall-security/src/main/java/com/macro/mall/security/config/SecurityConfig.java
+++ mall-security/src/main/java/com/macro/mall/security/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
@@ -37,37 +36,33 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = httpSecurity
-                .authorizeRequests();
-        //不需要保护的资源路径允许访问
-        for (String url : ignoreUrlsConfig.getUrls()) {
-            registry.antMatchers(url).permitAll();
-        }
-        //允许跨域请求的OPTIONS请求
-        registry.antMatchers(HttpMethod.OPTIONS)
-                .permitAll();
-        // 任何请求需要身份认证
-        registry.and()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                // 关闭跨站请求防护及不使用session
-                .and()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        httpSecurity
+                // 关闭跨站请求防护
+                .csrf(csrf -> csrf.disable())
+                // 不使用session
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(registry -> {
+                    // 不需要保护的资源路径允许访问
+                    for (String url : ignoreUrlsConfig.getUrls()) {
+                        registry.antMatchers(url).permitAll();
+                    }
+                    // 允许跨域请求的OPTIONS请求
+                    registry.antMatchers(HttpMethod.OPTIONS).permitAll();
+                    // 任何请求需要身份认证
+                    registry.anyRequest().authenticated();
+                })
                 // 自定义权限拒绝处理类
-                .and()
-                .exceptionHandling()
-                .accessDeniedHandler(restfulAccessDeniedHandler)
-                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .exceptionHandling(handling -> handling
+                        .accessDeniedHandler(restfulAccessDeniedHandler)
+                        .authenticationEntryPoint(restAuthenticationEntryPoint))
                 // 自定义权限拦截器JWT过滤器
-                .and()
-                .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                
+        ;
+        
         //有动态权限配置时添加动态权限校验过滤器
         if(dynamicSecurityService!=null){
-            registry.and().addFilterBefore(dynamicSecurityFilter, FilterSecurityInterceptor.class);
+            httpSecurity.addFilterBefore(dynamicSecurityFilter, FilterSecurityInterceptor.class);
         }
         return httpSecurity.build();
     }
